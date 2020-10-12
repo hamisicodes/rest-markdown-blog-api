d97c2e067055de10be6e536b3da24d22f17fd2cc
hamisicodes <newhamisi@gmail.com>
2020-10-12 20:59:38 +0300

    configure static files

--- blog/settings.py
+++ blog/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -112,10 +113,15 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS =[
+    os.path.join(BASE_DIR,'static')
+]
+STATIC_ROOT = os.path.join(BASE_DIR , 'static_root')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR , 'media_root')
+
 
 AUTHENTICATION_BACKENDS = [
 
@@ -136,7 +142,7 @@ REST_FRAMEWORK = {
 
 CORS_ALLOWED_ORIGINS = [
 
-    "http://localhost:3080",
+    "http://localhost:3000",
   
 ]
 
