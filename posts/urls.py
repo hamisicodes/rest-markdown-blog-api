df76c41e4ee5df904c4ec708c628ce0f17f2ab9b
hamisicodes <newhamisi@gmail.com>
2020-10-13 11:14:41 +0300

    implement Post Detail View and its url

--- posts/urls.py
+++ posts/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'posts'
 urlpatterns = [
     path('posts/', views.PostListView.as_view() , name='post_list'),
+    path('posts/<slug>', views.PostDetailView.as_view() , name='post_details'),
 
 ]
