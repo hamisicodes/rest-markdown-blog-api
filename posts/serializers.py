a9e090d2320a1843d7d1602258723bf6cf887e7d
hamisicodes <newhamisi@gmail.com>
2020-10-19 20:17:50 +0300

    set permissions for the view endpoints

--- posts/serializers.py
+++ posts/serializers.py
@@ -3,9 +3,26 @@ from .models import Post
 
 
 class PostSerializer(serializers.ModelSerializer):
+    is_author = serializers.SerializerMethodField()
     class Meta:
         model = Post
-        fields = "__all__"
+        fields = (
+
+            "title",
+            "slug",
+            "last_updated",
+            "content",
+            "thumbnail",
+            "is_author",
+
+        )
+    def get_is_author(self,obj):
+        request = self.context['request']
+        if request.user:
+            print(request.user)
+            return request.user == obj.user
+        else:
+            return False
 
 class PostCreateSerializer(serializers.ModelSerializer):
     class Meta:
--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,5 @@
 from rest_framework import generics, permissions
-
+from .permissions import isAuthor
 from .models import Post
 from .serializers import PostCreateSerializer, PostSerializer , PostUpdateSerializer
 
@@ -15,21 +15,29 @@ class PostDetailView(generics.RetrieveAPIView):
     serializer_class = PostSerializer
     lookup_field = 'slug'
 
+    def get_serializer_context(self):
+        context = super(PostDetailView,self).get_serializer_context()
+        context.update({
+            "request":self.request
+        })
+
+        return context
+
 class PostCreateView(generics.CreateAPIView):
-    permission_classes =[permissions.AllowAny]
+    permission_classes =[permissions.IsAuthenticated]
     serializer_class = PostCreateSerializer
 
     def perform_create(self,serializer):
         serializer.save(user = self.request.user)
 
 class PostUpdateView(generics.UpdateAPIView):
-    permission_classes =[permissions.AllowAny]
+    permission_classes =[permissions.IsAuthenticated,isAuthor]
     serializer_class = PostUpdateSerializer
     queryset = Post.objects.all()
     lookup_field = 'slug'
 
 class PostDeleteView(generics.DestroyAPIView):
-    permission_classes =[permissions.AllowAny]
+    permission_classes =[permissions.IsAuthenticated,isAuthor]
     queryset = Post.objects.all()
     lookup_field = 'slug'
 
